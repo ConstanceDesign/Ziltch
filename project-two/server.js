23d4f7282a4dadb8f7608a2076222b93cfc1cce5
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-07 00:31:32 -0500

    Add files via upload

---
+++ project-two/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+// Setup Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => 
+      console.log(`App listening on port ${PORT}!`));
+    });
